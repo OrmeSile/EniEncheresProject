c00a66f2354f19ea9ca665c26419bceea4558b60
mchatry2022 <mchatry2022@16SE44-71J3P02.ad.campus-eni.fr>
2023-01-30 17:08:49 +0100

    creation retrait bo

--- src/main/java/fr/eni/ecole/encheres/bo/Retrait.java
+++ src/main/java/fr/eni/ecole/encheres/bo/Retrait.java
@@ -1,4 +1,74 @@
 package fr.eni.ecole.encheres.bo;
 
 public class Retrait {
+	private int noArticle;
+	String rue;
+	String codePostal;
+	String ville;
+	
+	public Retrait(int noArticle, String rue, String codePostal, String ville) {
+		super();
+		this.noArticle = noArticle;
+		this.rue = rue;
+		this.codePostal = codePostal;
+		this.ville = ville;
+	}
+
+	public Retrait(String rue, String codePostal, String ville) {
+		super();
+		this.rue = rue;
+		this.codePostal = codePostal;
+		this.ville = ville;
+	}
+
+	public Retrait() {
+		super();
+	}
+
+	public int getNoArticle() {
+		return noArticle;
+	}
+
+	public void setNoArticle(int noArticle) {
+		this.noArticle = noArticle;
+	}
+
+	public String getRue() {
+		return rue;
+	}
+
+	public void setRue(String rue) {
+		this.rue = rue;
+	}
+
+	public String getCodePostal() {
+		return codePostal;
+	}
+
+	public void setCodePostal(String codePostal) {
+		this.codePostal = codePostal;
+	}
+
+	public String getVille() {
+		return ville;
+	}
+
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+
+	@Override
+	public String toString() {
+		return "Retrait [rue=" + rue + ", codePostal=" + codePostal + ", ville=" + ville + "]";
+	}
+	
+	
+	
+	
 }
+/*CREATE TABLE RETRAITS (
+		no_article       INTEGER NOT NULL,
+	    rue              VARCHAR(30) NOT NULL,
+	    code_postal      VARCHAR(15) NOT NULL,
+	    ville            VARCHAR(30) NOT NULL
+	)*/
