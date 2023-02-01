9affeb46316577fb2f7e3ae54f3100a6c4bff7c0
mchatry2022 <mchatry2022@16SE44-71J3P02.ad.campus-eni.fr>
2023-02-01 17:28:17 +0100

    creation methode dans dal insert article vendu

--- src/main/java/fr/eni/ecole/encheres/dal/jdbc/ArticleJDBC.java
+++ src/main/java/fr/eni/ecole/encheres/dal/jdbc/ArticleJDBC.java
@@ -1,15 +1,20 @@
 package fr.eni.ecole.encheres.dal.jdbc;
 
+import fr.eni.ecole.encheres.BusinessException;
 import fr.eni.ecole.encheres.bo.ArticleVendu;
 import fr.eni.ecole.encheres.dal.ConnectionProvider;
 import fr.eni.ecole.encheres.dal.DAO;
 
+import java.beans.Statement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
-public class ArticleJDBC implements DAO <ArticleVendu>{
+public class ArticleJDBC implements DAO<ArticleVendu> {
 	private final String INSERT = "INSERT INTO ARTICLES_VENDUS('nom_article','description',date_debut_enchere,date_fin_enchere, prix_initial, prix_vente, no_utilisateur, no_categorie, 'etat_vente', image ) values ?,?,?,?,?,?,?,?,?,?";
+
 	@Override
 	public ArticleVendu getOneById(int id) {
 		return null;
@@ -21,22 +26,31 @@ public class ArticleJDBC implements DAO <ArticleVendu>{
 	}
 
 	@Override
-	public ArticleVendu insert(ArticleVendu object) {
+	public ArticleVendu insert(ArticleVendu object)throws BusinessException{
+		BusinessException ex = new BusinessException();
 		try(Connection con = ConnectionProvider.getConnection()){
-			PreparedStatement ps = con.prepareStatement(INSERT);
+			PreparedStatement ps = con.prepareStatement(INSERT,java.sql.Statement.RETURN_GENERATED_KEYS);
 			ps.setString(1, object.getNomArticle());
 			ps.setString(2, object.getDescription());
 			ps.setTimestamp(3, java.sql.Timestamp.valueOf(object.getDateDebutEnchere()));
 			ps.setTimestamp(4, java.sql.Timestamp.valueOf(object.getDateFinEnchere()));
 			ps.setInt(5, object.getPrixInitial());
 			ps.setInt(6, object.getPrixVente());
-			ps.setInt(7, object.getUtilisateur());
-			ps.setString(8, object.getcategorieArticle());
+			ps.setInt(7, object.getUtilisateur().getNoUtilisateur());
+			ps.setInt(8, object.getcategorieArticle().getId());
 			ps.setString(9, object.getEtatVente());
 			ps.setString(10, object.getImage());
-			
+			ResultSet rs = ps.executeQuery();
+			if(rs.next()) {
+				object.setNoArticle(rs.getInt(1));
+				return object;
+			}else{
+				ex.addExceptionMessage("Erreur d'insertion");
+				throw ex;
+			}
+		}catch(SQLException e) {
+			ex.addExceptionMessage(e.getMessage());
+			throw ex;
 		}
-		return new ArticleVendu();
 	}
-	
 }
