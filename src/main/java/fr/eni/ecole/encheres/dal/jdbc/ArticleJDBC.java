a51ae7f95ac3892b3960b08db7a5fadfec1d1e0c
mchatry2022 <mchatry2022@16SE44-71J3P02.ad.campus-eni.fr>
2023-02-01 16:41:58 +0100

    ok

--- src/main/java/fr/eni/ecole/encheres/dal/jdbc/ArticleJDBC.java
+++ src/main/java/fr/eni/ecole/encheres/dal/jdbc/ArticleJDBC.java
@@ -1,11 +1,15 @@
 package fr.eni.ecole.encheres.dal.jdbc;
 
 import fr.eni.ecole.encheres.bo.ArticleVendu;
+import fr.eni.ecole.encheres.dal.ConnectionProvider;
 import fr.eni.ecole.encheres.dal.DAO;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
 
 public class ArticleJDBC implements DAO <ArticleVendu>{
+	private final String INSERT = "INSERT INTO ARTICLES_VENDUS('nom_article','description',date_debut_enchere,date_fin_enchere, prix_initial, prix_vente, no_utilisateur, no_categorie, 'etat_vente', image ) values ?,?,?,?,?,?,?,?,?,?";
 	@Override
 	public ArticleVendu getOneById(int id) {
 		return null;
@@ -18,6 +22,21 @@ public class ArticleJDBC implements DAO <ArticleVendu>{
 
 	@Override
 	public ArticleVendu insert(ArticleVendu object) {
-		return null;
+		try(Connection con = ConnectionProvider.getConnection()){
+			PreparedStatement ps = con.prepareStatement(INSERT);
+			ps.setString(1, object.getNomArticle());
+			ps.setString(2, object.getDescription());
+			ps.setTimestamp(3, java.sql.Timestamp.valueOf(object.getDateDebutEnchere()));
+			ps.setTimestamp(4, java.sql.Timestamp.valueOf(object.getDateFinEnchere()));
+			ps.setInt(5, object.getPrixInitial());
+			ps.setInt(6, object.getPrixVente());
+			ps.setInt(7, object.getUtilisateur());
+			ps.setString(8, object.getcategorieArticle());
+			ps.setString(9, object.getEtatVente());
+			ps.setString(10, object.getImage());
+			
+		}
+		return new ArticleVendu();
 	}
+	
 }
