c5453955da575d5f2b934207fb872f9a3b73d17c
OrmeSile <95030000+OrmeSile@users.noreply.github.com>
2023-02-01 23:19:07 +0100

    home setup

---
+++ encheres/src/main/java/fr/eni/ecole/encheres/dal/jdbc/UtilisateurJDBC.java
@@ -0,0 +1,66 @@
+package fr.eni.ecole.encheres.dal.jdbc;
+
+import fr.eni.ecole.encheres.BusinessException;
+import fr.eni.ecole.encheres.bo.Utilisateur;
+import fr.eni.ecole.encheres.dal.ConnectionProvider;
+import fr.eni.ecole.encheres.dal.DAOUtilisateur;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class UtilisateurJDBC implements DAOUtilisateur {
+	private final String LOGIN = "select no_utilisateur, nom, prenom, email, telephone, rue, code_postal, ville, credit, administrateur from UTILISATEURS u WHERE pseudo = ? AND mot_de_passe = ?";
+	private final String UTILISATEUR = "INSERT INTO UTILISATEURS VALUES ( N'pseudo', N'nom', N'prenom', N'email', N'telephone', N'rue', N'code_postal', N'ville', N'mot_de_passe', credit, administrateur)";
+	@Override
+	public Utilisateur getOneById(int id) {
+		return null;
+	}
+
+	@Override
+	public ArrayList<Utilisateur> getAll() {
+		return null;
+	}
+
+	@Override
+	public Utilisateur insert(Utilisateur object) {
+		return null;
+	}
+
+	public Utilisateur seConnecter(String pseudo, String motDePasse) throws BusinessException {
+		BusinessException ex = new BusinessException();
+		try(Connection con = ConnectionProvider.getConnection()){
+			PreparedStatement ps = con.prepareStatement(LOGIN);
+			ps.setString(1, pseudo);
+			ps.setString(2, motDePasse);
+			ResultSet rs = ps.executeQuery();
+			if(rs.next()){
+				int id = rs.getInt(1);
+				String nom = rs.getString(2);
+				String prenom = rs.getString(3);
+				String email = rs.getString(4);
+				String telephone = rs.getString(5);
+				String rue = rs.getString(6);
+				String codePostal = rs.getString(7);
+				String ville = rs.getString(8);
+				int credit = rs.getInt(9);
+				boolean administrateur = rs.getBoolean(10);
+				return new Utilisateur(id, pseudo, nom, prenom, email, telephone, rue, codePostal, ville, motDePasse, credit, administrateur);
+			}else{
+				ex.addExceptionMessage("Erreur de connection");
+				throw ex;
+			}
+		} catch (SQLException e) {
+			ex.addExceptionMessage(e.getMessage());
+			throw ex;
+		}
+	}
+
+	@Override
+	public Utilisateur insert(Utilisateur object) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
---
+++ encheres/src/main/webapp/WEB-INF/monProfilPage/monProfil.jsp
@@ -0,0 +1,43 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<jsp:include page="/WEB-INF/fragments/_bootstrap-import.jsp" />
+<title>Mon profil</title>
+</head>
+<body>
+	<div class="container">
+	
+		<header class="row navbar navbar-expand-md bg-body-tertiary">
+			<jsp:include page="/WEB-INF/fragments/_header.jsp" />
+		</header>
+
+		<h1 align="center">Mon profil</h1>
+
+				<!-- TODO: remplir le path de l'action vers la liste des enchères -->
+				<form method="post" action="${pageContext.request.contextPath}">
+					<label>Pseudo :</label><input type="text" id="pseudo" name="pseudo"/>
+					<label> Nom :</label><input type="text" id="nom" name="nom"/>
+					<br>
+					<label> Prénom :</label><input type="text" id="prenom" name="prenom"/>
+					<label> Email :</label><input type="email" id="email" name="email"/>
+					<br>
+					<label> Téléphone :</label><input type="tel" id="telephone" name="telephone" />
+					<label> Rue :</label><input type="text" id="rue" name="rue"/>
+					<br>
+					<label> Code Postal :</label><input type="text" id="codePostal" name="codePostal"/>
+					<label> Ville :</label><input type="text" id="ville" name="ville"/>
+					<br>
+					<label> Mot de passe :</label><input type="password" id="motDePasse" name="motDePasse"/>
+					<label> Confirmation :</label><input type="password" id="confirmationMotDePasse" name="confirmationMotDePasse"/>
+					<input type="submit" value="Créer"/>
+				</form>
+		
+		<br>
+		
+			<a href=${pageContext.request.contextPath}><button class="shadow__btn">Annuler</button></a>
+	</div>
+</body>
+</html>
