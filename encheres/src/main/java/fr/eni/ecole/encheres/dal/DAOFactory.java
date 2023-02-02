ff4427b633a46be75564eb75a8c09e16afafbd8b
OrmeSile <95030000+OrmeSile@users.noreply.github.com>
2023-02-02 01:18:22 +0100

    JDBC empty templates

--- encheres/src/main/java/fr/eni/ecole/encheres/dal/DAOFactory.java
+++ encheres/src/main/java/fr/eni/ecole/encheres/dal/DAOFactory.java
@@ -1,9 +1,18 @@
 package fr.eni.ecole.encheres.dal;
 
+import fr.eni.ecole.encheres.bo.ArticleVendu;
+import fr.eni.ecole.encheres.bo.Categorie;
+import fr.eni.ecole.encheres.bo.Enchere;
+import fr.eni.ecole.encheres.bo.Retrait;
 import fr.eni.ecole.encheres.dal.jdbc.UtilisateurJDBC;
 
 public class DAOFactory {
 	public static DAOUtilisateur getUtilisateurDAO(){
 		return new UtilisateurJDBC();
 	}
+//	TODO: uncomment and implement
+//	public static DAO<ArticleVendu> getArticleDAO();
+//	public static DAO<Categorie> getCategorieDAO();
+//	public static DAO<Enchere> getEnchereDAO();
+//	public static DAO<Retrait> getRetraitDAO();
 }
