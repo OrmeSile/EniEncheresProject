a02af1adffa203e5df263bd9355a069f933a27a0
OrmeSile <spamoremas@gmail.com>
2023-02-02 12:28:21 +0100

    jdbc cleanup

--- src/main/java/fr/eni/ecole/encheres/dal/UserFetchable.java
+++ src/main/java/fr/eni/ecole/encheres/dal/UserFetchable.java
@@ -1,7 +1,9 @@
 package fr.eni.ecole.encheres.dal;
 
+import fr.eni.ecole.encheres.BusinessException;
+
 import java.util.ArrayList;
 
 public interface UserFetchable<T> {
-	public ArrayList<T> getAllByUserId(int id);
+	public ArrayList<T> getAllByUserId(int id) throws BusinessException;
 }
